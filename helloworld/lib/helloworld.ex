986fb1555953b3280e9df8de67c57f63f7a89f63
lukeyuan31 <1393499408@qq.com>
2019-08-27 22:56:40 -0400

    finish hello world

---
+++ helloworld/lib/helloworld.ex
@@ -0,0 +1,19 @@
+defmodule Helloworld do
+  @moduledoc """
+  Documentation for Helloworld.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Helloworld.hello()
+      :world
+
+  """
+  def hello do
+    #:world
+    IO.puts("Hello World")
+  end
+end
