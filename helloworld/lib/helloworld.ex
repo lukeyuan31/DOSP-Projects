5e93b8a77cd18b9ecedb31a26657f740b160ddd8
陆克渊 <lukeyuan@lukeyuandeMacBook-Pro.local>
2019-08-27 21:48:07 -0400

    First Commit

---
+++ helloworld/lib/helloworld.ex
@@ -0,0 +1,19 @@
+defmodule Helloworld do
+  @moduledoc """
+  Documentation for Helloworld.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Helloworld.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts("Hello World")
+    #:world
+  end
+end
